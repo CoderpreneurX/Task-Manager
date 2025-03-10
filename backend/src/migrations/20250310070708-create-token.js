231049c463fc76e08a2089462a8e425e8e9b692f
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-10 13:53:01 +0530

    Created migrations

---
+++ backend/src/migrations/20250310070708-create-token.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.UUID
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      purpose: {
+        type: Sequelize.ENUM("email_verification", "password_reset")
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tokens');
+  }
+};
