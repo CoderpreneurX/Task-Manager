6add4e2b2f85f2d28a0a3056d644bbadb8b78b04
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-03-12 11:34:06 +0530

    Configured root url as "/tasks" instead of "/"

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/tasks',
+        permanent: true, // Set to `false` if you want a temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
